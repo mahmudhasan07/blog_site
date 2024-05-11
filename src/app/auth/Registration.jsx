493018d29bc881d03c3d5389d713193581f3cede
Hello CoderBoy <mahmudhasan.hb@gmail.com>
2024-05-12 02:57:18 +0600

    create registration from

--- src/app/auth/Registration.jsx
+++ src/app/auth/Registration.jsx
@@ -1,15 +1,13 @@
-'use client'
+"use client"
 import React, { useEffect } from 'react';
-// import { useForm } from 'react-hook-form';
-import {useForm}  from 'react-hook-form';
-import { FcGoogle } from "react-icons/fc";
+import { useForm } from 'react-hook-form';
 
-const Login = () => {
+const Registration = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
     useEffect(() => {
-        document.getElementById("login-from").style.rotate="180deg"
-        document.getElementById("registration-from").style.rotate="0deg"
+        document.getElementById("login-from").style.rotate = "180deg"
+        document.getElementById("registration-from").style.rotate = "0deg"
     }, []);
     return (
         <div id='login-from' className='border-2 w-1/3 p-3 h-96 my-auto absolute left-1/3 top-10'>
@@ -18,20 +16,20 @@ const Login = () => {
                 {/* register your input into the hook by invoking the "register" function */}
                 <div className='border-2 w-9/12 mx-auto'>
                     <label className='font-semibold text-lg'>Your Email</label> <br />
-                <input className='border-2 w-80 border-black p-1 rounded-xl' defaultValue="" {...register("email")} />
+                    <input className='border-2 w-80 border-black p-1 rounded-xl' defaultValue="" {...register("email")} />
                 </div>
 
                 {/* include validation with required or other standard HTML validation rules */}
                 <div className='border-2 w-9/12 mx-auto'>
-                <label className='font-semibold text-lg'>Your Password</label> <br />
-                <input className='border-2 w-80 border-black p-1 rounded-xl' {...register("password", { required: true })} />
+                    <label className='font-semibold text-lg'>Your Password</label> <br />
+                    <input className='border-2 w-80 border-black p-1 rounded-xl' {...register("password", { required: true })} />
                 </div>
                 {/* errors will return when field validation fails  */}
                 {errors.exampleRequired && <span>This field is required</span>}
 
                 <div className='text-center'>
                     <p className='text-lg font-bold text-end'>New User ??</p>
-                <input className='btn' type="submit" />
+                    <input className='btn' type="submit" />
                 </div>
             </form>
             <div>
@@ -42,4 +40,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default Registration;
