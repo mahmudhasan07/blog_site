010f79f44ed2b9a5b8ff8bee9db57e128a0aed37
Hello CoderBoy <mahmudhasan.hb@gmail.com>
2024-05-29 19:35:01 +0600

    working on lifestyle

--- src/app/components/lifestyle/Lifestyle.jsx
+++ src/app/components/lifestyle/Lifestyle.jsx
@@ -1,14 +1,15 @@
+"use-client"
 import React from 'react';
-
+import  "./Lifestyle.scss";
 const Lifestyle = () => {
     return (
         <section className='my-10'>
-            <div className='text-center'>
+            <div id='title' className='text-center'>
                 <h1 className='text-5xl font-extrabold'>Life Style</h1>
                 <p className='text-xl font-semibold'>Explore World with Us</p>
             </div>
             <div>
-
+                
             </div>
 
         </section>
