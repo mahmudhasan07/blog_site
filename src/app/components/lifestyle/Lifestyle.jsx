89c1aaef9381376a7be5551b86543cb22a05a7a9
Hello CoderBoy <mahmudhasan.hb@gmail.com>
2024-05-30 22:24:35 +0600

    working on categories of travel

--- src/app/components/lifestyle/Lifestyle.jsx
+++ src/app/components/lifestyle/Lifestyle.jsx
@@ -1,15 +1,28 @@
-"use-client"
+// "use-client"
 import React from 'react';
 import  "./Lifestyle.scss";
 const Lifestyle = () => {
     return (
-        <section className='my-10'>
+        <section className='my-10 space-y-5'>
             <div id='title' className='text-center'>
                 <h1 className='text-5xl font-extrabold'>Life Style</h1>
                 <p className='text-xl font-semibold'>Explore World with Us</p>
             </div>
-            <div>
-                
+            <div className='space-y-5'>
+                <div  className='flex justify-center  gap-14'>
+                    <div id='div1' className='relative  my-auto rounded-lg bg-black'>
+                    <img src='https://i.ibb.co/h8t1yJY/historical.jpg' className='w-[500px] rounded-lg'></img>
+                    <p className=' top-1/2 text-4xl font-extrabold left-1/4 text-white'>History Places</p>
+                    </div>
+                    <img src='https://i.ibb.co/vQsY3tG/relaxtravel.jpg' className='w-[500px] rounded-lg'></img>
+                </div>
+                <div>
+                <div className='flex justify-center  gap-10'>
+                    <img src='https://i.ibb.co/jvnPgW3/City.jpg' className='w-[500px] my-auto h-fit rounded-lg'></img>
+                    <img src='https://i.ibb.co/q0t9bkm/adventure.png' className='w-96 my-auto rounded-lg'></img>
+                    <img src='https://i.ibb.co/y414jgN/ecotourism.jpg' className='w-[500px] my-auto h-fit rounded-lg'></img>
+                </div>
+                </div>
             </div>
 
         </section>
