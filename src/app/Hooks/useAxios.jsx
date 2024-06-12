3ffc44581a4ede4797da0b7d36f04e07d193b297
Hello CoderBoy <mahmudhasan.hb@gmail.com>
2024-06-13 02:28:46 +0600

    working on redux

--- src/app/Hooks/useAxios.jsx
+++ src/app/Hooks/useAxios.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 
 export const AxiosSource = axios.create({
     baseURL: 'https://watch-server-07.vercel.app',
+    withCredentials: true
 
-  });
+});
 
 const useAxios = () => {
     return AxiosSource
