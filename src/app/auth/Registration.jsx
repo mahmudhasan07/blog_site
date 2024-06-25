cf59ffbbf5e5bcc52e7e05bd8c9ad6831bf9a5eb
Hello CoderBoy <mahmudhasan.hb@gmail.com>
2024-06-25 14:30:05 +0600

    test work for confirm auto registration after signUp

--- src/app/auth/Registration.jsx
+++ src/app/auth/Registration.jsx
@@ -37,6 +37,7 @@ const Registration = () => {
                 }
                 else {
                     console.log(result);
+                    result.userConfirmed(true)
                 }
             })
 
--- src/app/components/Redux/ReduxFuntion.jsx
+++ src/app/components/Redux/ReduxFuntion.jsx
@@ -1,36 +1,62 @@
+import useAuth from '@/app/auth/useAuth'
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { AuthenticationDetails, CognitoUser } from 'amazon-cognito-identity-js'
 
 const initialState = {
   value: 0,
 }
 
 
-const CreateUser= createAsyncThunk(
-    "createUser",
-    async()=>{
-        
-    }
+const CreateUser = createAsyncThunk(
+  "createUser",
+  async (email, password, Attributes) => {
+    useAuth.signUp(email, password, Attributes, null, (err, res) => {
+      if (err) {
+        return err
+      }
+      return res
+      
+    })
+  }
+)
+
+
+const LogInUser = createAsyncThunk(
+  "LoginUser",
+  async (email, password) => {
+    const User = new CognitoUser({
+      Pool: useAuth,
+      Username: email
+    })
+    const getUser = new AuthenticationDetails({
+      Username: email,
+      Password: password
+    })
+
+    User.confirmRegistration()
+
+    User.authenticateUser(getUser, ({
+      onSuccess: (res) => {
+        return res
+      },
+      onFailure: (err) => {
+        return err
+      }
+    }))
+  }
 )
 
 export const counterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
-    increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1
-    },
-    decrement: (state) => {
-      state.value -= 1
-    },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload
-    },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase()
+  }
 })
+
 
 // Action creators are generated for each case reducer function
 export const { increment, decrement, incrementByAmount } = counterSlice.actions
