09b72702fc75a7d43f787f681b433bc30b80d971
Hello CoderBoy <mahmudhasan.hb@gmail.com>
2024-07-08 01:58:38 +0600

    added new data

--- src/app/add-blog/Blog.jsx
+++ src/app/add-blog/Blog.jsx
@@ -1,22 +1,33 @@
 "use client"
 import React, { useState } from 'react';
-import {} from "./Blog.css"
+import { } from "./Blog.css"
+import axios from 'axios';
 
 const Blog = () => {
     const [previewImage, setPreviewImage] = useState([]);
+    const [hostImages, sethostImages] = useState([]);
 
     const handleImage = (e) => {
         e.preventDefault()
         const file = e.target.files
         const ImageArray = Array.from(file)
         const preview = ImageArray.map(element => URL.createObjectURL(element))
-        const hostImage = ImageArray.map(e=> {
+        ImageArray.map(e => {
             const fromData = new FormData()
-            fromData.append("files", e)
-            fromData.append("us")
+            fromData.append("file", e)
+            fromData.append("upload_preset", 'blog_images')
+            axios.post('https://api.cloudinary.com/v1_1/daudgshta/upload', fromData)
+                .then(res => {
+                    console.log(res);
+                    sethostImages(e => e.concat(res))
+                })
+                .catch(err => {
+                    console.log(err);
+                })
         })
         // console.log(preview);
         setPreviewImage(data => data.concat(preview))
+        console.log(hostImages);
 
     }
     // console.log(previewImage);
@@ -37,12 +48,12 @@ const Blog = () => {
             <div>
                 <label className="text-lg font-semibold">Preview Your Photos</label>
                 <div className='flex gap-3'>
-                {
-                    previewImage.length > 0 ?
-                        previewImage.map((element, idx) => <img key={idx} src={element} className='w-20 h-20 object-cover'></img>)
-                        :
-                        <p className="text-[#6f53d6] font-semibold">No Image Preview</p>
-                }
+                    {
+                        previewImage.length > 0 ?
+                            previewImage.map((element, idx) => <img key={idx} src={element} className='w-20 h-20 object-cover'></img>)
+                            :
+                            <p className="text-[#6f53d6] font-semibold">No Image Preview</p>
+                    }
                 </div>
             </div>
             <div>
@@ -50,7 +61,7 @@ const Blog = () => {
                 <textarea className="border-2 w-full border-black p-2 rounded-2xl " rows={'7'}></textarea>
             </div>
             <div className='mx-auto w-fit'>
-                <button  id='button_Submit' className=' border-2 p-2 text-white mt-3 border-white text-lg rounded-2xl font-semibold'>Submit Your Blog</button>
+                <button id='button_Submit' className=' border-2 p-2 text-white mt-3 border-white text-lg rounded-2xl font-semibold'>Submit Your Blog</button>
             </div>
         </form>
     );
